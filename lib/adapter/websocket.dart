b2ad1dcaab9b1fd5cc14a6bf5c5ed51f46a0bc83
Iori Ichinose <191180103@smail.nju.edu.cn>
2021-10-27 22:11:00 +0800

    feat: implements websocket command and response types

---
+++ lib/adapter/websocket.dart
@@ -0,0 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'websocket.g.dart';
+
+@JsonSerializable()
+class Command {
+  int syncId;
+  String command;
+  String? subCommand;
+  Map<String, dynamic> content;
+
+  Command(
+      {required this.syncId,
+      required this.command,
+      this.subCommand,
+      required this.content});
+
+  factory Command.fromJson(json) => _$CommandFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CommandToJson(this);
+}
+
+@JsonSerializable()
+class Response {
+  int syncId;
+  Map<String, dynamic> content;
+
+  Response({required this.syncId, required this.content});
+
+  factory Response.fromJson(json) => _$ResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ResponseToJson(this);
+}
---
+++ lib/adapter/websocket.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'websocket.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Command _$CommandFromJson(Map<String, dynamic> json) => Command(
+      syncId: json['syncId'] as int,
+      command: json['command'] as String,
+      subCommand: json['subCommand'] as String?,
+      content: json['content'] as Map<String, dynamic>,
+    );
+
+Map<String, dynamic> _$CommandToJson(Command instance) => <String, dynamic>{
+      'syncId': instance.syncId,
+      'command': instance.command,
+      'subCommand': instance.subCommand,
+      'content': instance.content,
+    };
+
+Response _$ResponseFromJson(Map<String, dynamic> json) => Response(
+      syncId: json['syncId'] as int,
+      content: json['content'] as Map<String, dynamic>,
+    );
+
+Map<String, dynamic> _$ResponseToJson(Response instance) => <String, dynamic>{
+      'syncId': instance.syncId,
+      'content': instance.content,
+    };
